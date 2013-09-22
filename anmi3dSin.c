3bb43f8e97e11cb24fb9f9ba59859854ae3bd11e
johnuiterwyk <johnuiterwyk@gmail.com>
2013-09-22 15:05:45 +1000

    added apple ifdef

--- anmi3dSin.c
+++ anmi3dSin.c
@@ -5,9 +5,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glut.h>
+#ifdef __APPLE__
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glu.h>
+#  include <GLUT/glut.h>
+#else
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+#  include <GL/glut.h>
+#endif
 
 #include <math.h>
 
