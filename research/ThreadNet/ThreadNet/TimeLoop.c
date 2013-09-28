a87e22263d582e197f5806de112822e1806f93ec
johnuiterwyk <johnuiterwyk@gmail.com>
2013-09-28 20:44:45 +1000

    Added TimeLoop library

---
+++ research/ThreadNet/ThreadNet/TimeLoop.c
@@ -0,0 +1,80 @@
+//
+//  main.c
+//  SocketTest
+//
+//  Created by John Uiterwyk on 8/18/13.
+//  Copyright (c) 2013 John Uiterwyk. All rights reserved.
+//
+#include "TimeLoop.h"
+
+TimeLoop_Data *
+TimeLoop_Init()
+{
+    TimeLoop_Data *data;
+    data = calloc(1, sizeof(TimeLoop_Data));
+    if(data == NULL)
+    {
+        perror("Calloc error in TimeLoop_Init");
+        exit(EXIT_FAILURE);
+    }
+    data->tickCount = 0;
+    data->interval = 1;
+    data->interrupt = 0;
+    gettimeofday(&(data->start), NULL);
+    gettimeofday(&(data->current), NULL);
+    gettimeofday(&(data->previous), NULL);
+    return data;
+}
+
+void
+TimeLoop_Start(TimeLoop_Data * timeLoopData,void (* tickFunc)(void*),void * tickFuncData)
+{
+    do {
+        /*get current time */
+        gettimeofday(&timeLoopData->current, NULL);
+        
+        
+        TimeLoop_Compare(&timeLoopData->offset, &timeLoopData->current, &timeLoopData->previous);
+        TimeLoop_Compare(&timeLoopData->total, &timeLoopData->current, &timeLoopData->start);
+        
+        tickFunc(tickFuncData);
+        
+        timeLoopData->tickCount++;
+        timeLoopData->previous = timeLoopData->current;
+        TimeLoop_PrintTime(timeLoopData);
+        
+        usleep(timeLoopData->interval * 1000000);
+        
+    } while (timeLoopData->interrupt != 1);
+}
+
+void
+TimeLoop_PrintTime(TimeLoop_Data * data)
+{
+    printf("tick:%d offset: %ld.%06d, total: %ld.%06d \n",
+           data->tickCount,
+           data->offset.tv_sec,
+           data->offset.tv_usec,
+           data->total.tv_sec,
+           data->total.tv_usec);
+}
+long int TimeLoop_TimeToSleep(TimeLoop_Data * data)
+{
+    long int result = data->interval * 2 * 1000000
+    - (data->offset.tv_usec + 1000000 * data->offset.tv_sec);
+    return result;
+    
+    
+}
+int TimeLoop_Compare(timev *result, timev *t1, timev *t2)
+{
+    long int diff = (t1->tv_usec + 1000000 * t1->tv_sec)
+    - (t2->tv_usec + 1000000 * t2->tv_sec);
+    
+    result->tv_sec = diff / 1000000;
+    result->tv_usec = diff % 1000000;
+    
+    if(diff>0)return 1;
+    if(diff<0)return -1;
+    if(diff==0)return 0;
+}
---
+++ research/ThreadNet/ThreadNet/main.c
@@ -0,0 +1,42 @@
+//
+//  main.c
+//  ThreadNet
+//
+//  Created by John Uiterwyk on 9/27/13.
+//  Copyright (c) 2013 John Uiterwyk. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include "TimeLoop.h"
+typedef struct test
+{
+    char message[20];
+    int count;
+}Test_Data;
+
+void TestFunction(void * data)
+{
+    Test_Data * testData = (Test_Data *)data;
+    testData->count++;
+    printf("%s %d\n",testData->message,testData->count);
+}
+
+int main(int argc, const char * argv[])
+{
+
+    Test_Data testData;
+    TimeLoop_Data * timeLoopData;
+    
+    strcpy(testData.message,"Hello World!");
+    testData.count= 0;
+    
+    timeLoopData = TimeLoop_Init();
+    timeLoopData->interval = 1;
+    
+    TimeLoop_Start(timeLoopData, &TestFunction, &testData);
+    
+    
+    return 0;
+}
+
