65d41f32ec62479fedcc25cbe9b6886079c0dfd1
Virginia King <ginnery@gmail.com>
2013-09-29 18:52:56 +1000

    changing display to use openVG primitives

--- src/display.h
+++ src/display.h
@@ -36,7 +36,7 @@ typedef struct
 {
   uint32_t screen_width;
   uint32_t screen_height;
-  // OpenGL|ES objects
+  /* OpenGL|ES objects*/
   EGLDisplay display;
   EGLSurface surface;
   EGLContext context;
